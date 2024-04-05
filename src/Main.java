ed39083ac0ee2ed425a519506ba856fb5d3ab068
akashKarmakar02 <akash.anan03@gmail.com>
2024-04-05 21:25:06 +0530

    java 22 commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
-import RapidWeb.server.RapidWebServer;
-import data.DataContext;
+import RapidJWeb.server.RapidWebServer;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+import java.io.IOException;
 
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        var server = new RapidWebServer(8080);
+
+        server.get("/", (request, response) -> {
+            var person = new Person("Akash", 21);
+            response.render("index", person);
+        });
+
+        server.post("/", ((request, response) -> {
+            response.setResponse("Hello " + request.getBody().get("name"));
+        }));
+
+        server.get("/about", ((request, response) -> {
+            var person = new Person("Ananya", 3);
+            response.render("about", person);
+        }));
+
+        server.run();
     }
 }
